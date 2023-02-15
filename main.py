47c8228b346374fe2035ef17bcdaa38f5778f1c5
abdulqgg <abdul.gilani@gmail.com>
2023-02-15 22:11:05 +0000

    prettify

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.foodaidnetwork.org.uk/'
+url = 'https://www.google.com/maps/d/u/0/viewer?mid=15mnlXFpd8-x0j4O6Ck6U90chPn4bkbWz&ll=50.80559354911368%2C-1.470568783234869&z=7'
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 links =  soup.find_all('a')
 
-print(soup)
+print(soup.prettify())
 
 # example: print the text of all links on the page
 for link in links:
