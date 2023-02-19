e6e670e89bb46511b1a33d19d284e18f0481f27c
abdulqgg <abdul.gilani@gmail.com>
2023-02-19 01:37:14 +0000

    comments

--- main.py
+++ main.py
@@ -3,30 +3,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
-# Start a new Chrome browser window
+# Open chrome 
 driver = webdriver.Chrome()
 
-# Navigate to the webpage containing the element you want to extract
+# Destinated website
 driver.get("https://www.google.com/maps/d/viewer?mid=15mnlXFpd8-x0j4O6Ck6U90chPn4bkbWz&ll=51.89185325888764%2C1.659932842043248&z=8")
 
-# Wait for the page to load and display the element you want to extract
-# (you may need to adjust the wait time depending on the page load speed)
+# Wait for page to load
 driver.implicitly_wait(10)
 
-# Css selector
+# Css selectors
 csselector = "#legendPanel > div > div > div.i4ewOd-PBWx0c-bN97Pc-haAclf > div > div > div.i4ewOd-pbTTYe-haAclf > div > div > div.HzV7m-pbTTYe-bN97Pc > div.HzV7m-pbTTYe-JNdkSc > div.HzV7m-pbTTYe-JNdkSc-PntVL"
 dropdowncss = "#legendPanel > div > div > div.i4ewOd-PBWx0c-bN97Pc-haAclf > div > div > div.i4ewOd-pbTTYe-haAclf > div > div > div.HzV7m-pbTTYe-bN97Pc.HzV7m-pbTTYe-bN97Pc-qAWA2 > div.HzV7m-pbTTYe-KoToPc-ornU0b-haAclf > div > div"
 
-# Find the element using the CSS selector you provided
-#element = driver.find_element(By.CSS_SELECTOR, csselector)
-
-#dropdown element and click
+# Dropdown element and click
 dropdown = driver.find_element(By.CSS_SELECTOR, dropdowncss)
 ActionChains(driver).move_to_element(dropdown).click().perform()
 
+# Find the element 
 desired_element = driver.find_element(By.CSS_SELECTOR, csselector)
 
-# Extract the text content of the element
+# Extract text for element
 element_text = desired_element.text
 
 # Print the extracted text
