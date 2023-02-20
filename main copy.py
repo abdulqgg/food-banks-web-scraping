bfd06841f78d6a4f66a49c5fd6b8c9a9b8841e4b
abdulqgg <abdul.gilani@gmail.com>
2023-02-20 20:24:43 +0000

    foodbank_back function

--- main copy.py
+++ main copy.py
@@ -13,18 +13,22 @@ driver.get("https://www.google.com/maps/d/viewer?mid=15mnlXFpd8-x0j4O6Ck6U90chPn
 # Wait for page to load
 driver.implicitly_wait(10)
 
-# Opens drop down and expands first foodbank
+# Finds and opens dropdown
 actions = ActionChains(driver)
 dropdown = driver.find_element(By.XPATH, '//*[@id="legendPanel"]/div/div/div[2]/div/div/div[2]/div/div/div[3]/div[2]/div/div')
-foodbank = dropdown.find_element(By.XPATH, '//*[@id="legendPanel"]/div/div/div[2]/div/div/div[2]/div/div/div[3]/div[3]/div[2]/div[1]')
 actions.move_to_element(dropdown).click().perform()
-foodbank.click()
-time.sleep(2)
-back = driver.find_element(By.XPATH, '//*[@id="featurecardPanel"]/div/div/div[3]/div[1]/div/span/span/span')
-actions.move_to_element(back).click().perform()
-time.sleep(2)
 
+# Clicks on foodbank and goes back
+def foodbank_back(index):
+    foodbank = driver.find_element(By.XPATH, '//*[@id="legendPanel"]/div/div/div[2]/div/div/div[2]/div/div/div[3]/div[3]/div[2]/div['+str(index)+']')
+    foodbank.click()
+    time.sleep(2)
+    back = driver.find_element(By.XPATH, '//*[@id="featurecardPanel"]/div/div/div[3]/div[1]/div/span/span/span')
+    actions.move_to_element(back).click().perform()
+    time.sleep(2)
 
+foodbank_back(1)
+foodbank_back(6)
 
 # Close the browser window
 driver.quit()
