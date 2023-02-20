bc35b9e2dc424dbfe0130040b9769d9672205aa7
abdulqgg <abdul.gilani@gmail.com>
2023-02-20 20:31:31 +0000

    extracting infomation

--- main copy.py
+++ main copy.py
@@ -23,12 +23,20 @@ def foodbank_back(index):
     foodbank = driver.find_element(By.XPATH, '//*[@id="legendPanel"]/div/div/div[2]/div/div/div[2]/div/div/div[3]/div[3]/div[2]/div['+str(index)+']')
     foodbank.click()
     time.sleep(2)
+    for i in range(1,6):
+        get_info(i)
     back = driver.find_element(By.XPATH, '//*[@id="featurecardPanel"]/div/div/div[3]/div[1]/div/span/span/span')
     actions.move_to_element(back).click().perform()
     time.sleep(2)
 
+def get_info(index_info):
+    info = driver.find_element(By.XPATH, '//*[@id="featurecardPanel"]/div/div/div[4]/div[1]/div['+str(index_info)+']')
+    print(info.text)
+
 foodbank_back(1)
 foodbank_back(6)
+
+
 
 # Close the browser window
 driver.quit()
